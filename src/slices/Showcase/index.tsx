b129e377425ed4bf1b84c071e4235f5c94ce3533
solutionstack99 <solution.stack99@gmail.com>
2024-11-15 14:08:09 -0500

    Change background to green.

--- src/slices/Showcase/index.tsx
+++ src/slices/Showcase/index.tsx
@@ -26,7 +26,7 @@ const Showcase = ({ slice }: ShowcaseProps): JSX.Element => {
       data-slice-variation={slice.variation}
       className="relative"
     >
-      <div className="glow absolute -z-10 aspect-square w-full max-w-xl rounded-full bg-blue-400/20 blur-3xl filter" />
+      <div className="glow absolute -z-10 aspect-square w-full max-w-xl rounded-full bg-green-400/20 blur-3xl filter" />
 
       <PrismicRichText
         field={slice.primary.heading}
@@ -41,7 +41,7 @@ const Showcase = ({ slice }: ShowcaseProps): JSX.Element => {
 
       <div className="mt-16 grid items-center gap-8 rounded-xl border border-blue-50/20 bg-gradient-to-b from-slate-50/15 to-slate-50/5 px-8 py-8 backdrop-blur-sm lg:grid-cols-3 lg:gap-0 lg:py-12">
         <div>
-          <div className="w-fit rounded-lg bg-blue-500/35 p-4 text-3xl">
+          <div className="w-fit rounded-lg bg-green-500/35 p-4 text-3xl">
             <>{slice.primary.icon && icons[slice.primary.icon]}</>
           </div>
           <div className="mt-6 text-2xl font-normal">
